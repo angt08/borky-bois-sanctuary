a4030eda73093205233027ea1cb31311fbf8e3f5
ang <angiezafra07@gmail.com>
2020-02-28 17:48:27 -0500

    last commit for today

--- my-app/src/components/Pow.jsx
+++ my-app/src/components/Pow.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import field from '../banners/field.jpg';
-// import robert from '../public/images/robert.jpg';
+import robert from '../banners/robert.jpg';
 // my-app/public/images/robert.jpg
 export default function Pow(props) {
+
   return (
+    
     <div>
       <img id="field-banner" src={field} />
 
       <h3> Pooch of the week: </h3>
 
-      <div>
+      <div id="pow">
 
-        {/* <img id="robert" src={robert} /> */}
+        <img id="robert" src={robert} />
+        <div>
+          esgihseoighsifnardeoirsdgaemfsdfl;sfefkeskfeskflesf
+          efiejfisejfiseofjisefjsifjsiefjsei
+          <br/>
+        </div>
 
       </div>
     </div>
