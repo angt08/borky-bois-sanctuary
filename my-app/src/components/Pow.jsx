4677928e6abc2f99fdaf3c12c1349a49a22747b9
ang <angiezafra07@gmail.com>
2020-03-09 14:08:11 -0400

    fixing pow

--- my-app/src/components/Pow.jsx
+++ my-app/src/components/Pow.jsx
@@ -6,18 +6,31 @@ export default function Pow(props) {
 
   return (
 
-    <div >
+    <div>
       <img id="field-banner" src={field} />
+      <h3 id="pow-h3"> Pooch of the week: </h3>
 
 
       <div className="pow">
-      <h3> Pooch of the week: </h3>
         <img id="robert" src={robert} />
 
         <div id="pow-info">
+          <h5 id="pooch-description">
           I have ever seen doggo he made many woofs, noodle horse very jealous pupper doge.
           Big ol pupper what a nice floof h*ck you are doing me the shock heckin good boys and girls boof, corgo you are
           doing me a frighten long water shoob thicc floofs.
+          
+        <br />
+        
+          Age:7
+    
+        <br />
+        Breed: Black Lab
+    
+        <br />
+        Days in Sanctuary: 52
+   
+           </h5>
 
         </div>
 
