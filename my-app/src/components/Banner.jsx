e0afbb0dc36b78c0f38eaee3948be52dbf02f551
ang <angiezafra07@gmail.com>
2020-03-16 18:28:01 -0400

    still working on this, looking to add an API soon

--- my-app/src/components/Banner.jsx
+++ my-app/src/components/Banner.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import dogbeach from '../banners/dogbeach.jpg'
+// import dogbeach from '../banners/dogbeach.jpg'
 
 
 export default function Banner() {
   return (
     
     <div className="banner">
-    <img  id="borky-banner"  src={dogbeach} />
+    {/* <img  id="borky-banner"  src={dogbeach} /> */}
     <p>Adopt a Borky Boy</p>
-    <h4> All dogs listed are  ready for adoption</h4>
+      <h4> All dogs listed are  ready for adoption</h4>
+      
     </div> 
   )
 }
--- my-app/src/components/PowBanner.jsx
+++ my-app/src/components/PowBanner.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
-import field from '../banners/field.jpg';
+// import field from '../banners/field.jpg';
 
 export default function PowBanner() {
   return (
     <div className="banner">
-      <img  id="borky-banner"  src={field} />
-        <h4>
+      {/* <img id="borky-banner" src={field} /> */}
+      
+      <div>
+        <>
           Every week one of our dogs is chosen to be
           featured as pooch of the week!
           whether they've been extra awesome or even
           learned a new trick ,we want to make sure
           they are being recognized.
-        </h4>
+        </>
+      </div>
     
     </div>
   )
